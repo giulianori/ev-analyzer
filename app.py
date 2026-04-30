de352cde58dd29c8753caa91faaacc3a40b963cf
Giuliano <53431146+giulianori@users.noreply.github.com>
2026-04-30 15:18:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -162,18 +162,5 @@ else:
     st.info("Carica un file CSV per iniziare")
 
 
-    # =========================
-    # GRAFICO STORICO
-    # =========================
-    st.subheader("📈 Trend Consumo Reale")
-
-    fig2, ax = plt.subplots(figsize=(10,4))
-    ax.plot(storico.index + 1, storico["Consumo_reale_kWh_100km"], marker='o')
-    ax.set_xlabel("Viaggio #")
-    ax.set_ylabel("kWh/100km")
-    ax.grid()
-
-    st.pyplot(fig2)
-
-else:
-    st.info("Carica un file CSV per iniziare")
+   
+    
